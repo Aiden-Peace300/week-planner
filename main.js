04d4c09ce7cee71733e4ebe23b24607585d2ab61
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-08-04 22:02:01 +0000

    tried to make some javascript edits

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const $clickButtonList = document.querySelectorAll('.click-button');
 const $closeButton = document.querySelector('.close-button');
+
+const $form = document.querySelector('form');
+
 const $timeInput = document.querySelector('#time');
 const $dateInput = document.querySelector('#date');
+console.log($dateInput);
+
+function handleSubmit(event) {
+  if (event.target.getAttribute('class') === 'submit') {
+    // const $time = $form.elements.time.value;
+    // const $date = $form.elements.date.value;
+    console.log(event.target);
+  }
+}
+
+$form.addEventListener('click', handleSubmit);
 
 function clickButton() {
   popup.style.display = 'block';
