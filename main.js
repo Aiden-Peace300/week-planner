c070b7503e4c156120ec5de4c1547c39f5908d03
Aiden Peace <aidenpeace300@gmail.com>
2023-08-04 19:09:39 +0000

    added time and date for popup

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const $clickButton = document.querySelector('.click-button');
+const $clickButton = document.querySelectorAll('.click-button');
 const $closeButton = document.querySelector('.close-button');
 
-// when you click a date
+// when you click a day
 const popup = document.querySelector('.popup');
 function clickButton() {
   popup.style.display = 'block';
