2c3dbfde6c8392614ffd1a671558102987086a5f
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-04 18:39:04 +0000

    updated modal

---
+++ main.js
@@ -0,0 +1,13 @@
+const $clickButton = document.querySelector('.click-button');
+const $closeButton = document.querySelector('.closeButton');
+
+// when you click a date
+const popup = document.querySelector('.popup');
+function clickButton() {
+  popup.style.display = 'block';
+}
+function closeButton() {
+  popup.style.display = 'none';
+}
+$clickButton.addEventListener('click', clickButton);
+$closeButton.addEventListener('click', closeButton);
